ffa49e2845c26577b64cd870fb657bb14fac43a1
solar-citizen <fsknnn@gmail.com>
2021-05-09 00:48:23 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//Jquery method
+$(document).ready(function(){
+
+    //Slick Carousel
+    $('.responsive').slick({
+        mobileFirst: true,
+        autoplay: true,
+        dots: true,
+        //infinite: false,
+        speed: 400,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+        ]
+    });
+});
