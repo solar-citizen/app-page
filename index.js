6fb926a77f3c6fef63a9ec40f0ea871b91bcaa77
solar-citizen <fsknnn@gmail.com>
2021-05-10 18:27:57 +0300

    v.0.1 // dev branch init

--- index.js
+++ index.js
@@ -1,36 +1,124 @@
 //Jquery method
 $(document).ready(function(){
 
-    //Slick Carousel
-    $('.responsive').slick({
+    //Slick Carousel (TECH)
+    $('.tech .responsive').slick({
+        arrows: true,   
+        dots: false,                      //?????
         mobileFirst: true,
         autoplay: true,
-        dots: true,
         //infinite: false,
-        speed: 400,
+        speed: 500,
         slidesToShow: 4,
         slidesToScroll: 4,
         responsive: [{
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                arrows: true,
+                autoplay: true,
+                slidesToShow: 4,
+                slidesToScroll: 4,
                 infinite: true,
-                dots: true,
             }
         },
         {
             breakpoint: 600,
             settings: {
+                arrows: false,
+                autoplay: true,
                 slidesToShow: 2,
                 slidesToScroll: 2,
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 350,
             settings: {
+                arrows: false,
+                autoplay: true,
                 slidesToShow: 1,
                 slidesToScroll: 1,
+            }
+        }
+        ]
+    });
+
+    //Slick Carousel (SECURITY)
+    $('.security .responsive').slick({
+        arrows: true,   
+        dots: false,                      //?????
+        mobileFirst: true,
+        autoplay: true,
+        //infinite: false,
+        speed: 600,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                arrows: true,
+                autoplay: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 350,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+        ]
+    });
+
+    //Slick Carousel (FAVOURITE)
+    $('.favourite .responsive').slick({
+        arrows: false,   
+        dots: false,                      //?????
+        mobileFirst: true,
+        autoplay: true,
+        //infinite: false,
+        speed: 700,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                slidesToShow: 5,
+                slidesToScroll: 5,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 350,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                slidesToShow: 2,
+                slidesToScroll: 2,
             }
         }
         ]
